c43efdd072c69ddbbafc4dc451adfc2313bf41f8
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2017-02-18 13:04:28 +0000

    added two functions that read /dev/random

--- __init__.py
+++ __init__.py
@@ -9,5 +9,7 @@ the years.
 from .convert_bytes_to_pretty_bytes import convert_bytes_to_pretty_bytes
 from .does_file_have_RTP_hints import does_file_have_RTP_hints
 from .find_program_version import find_program_version
+from .generate_password import generate_password
+from .generate_random_stub import generate_random_stub
 from .is_moov_at_beginning_of_MP4 import is_moov_at_beginning_of_MP4
 from .print_MP4_atoms import print_MP4_atoms
