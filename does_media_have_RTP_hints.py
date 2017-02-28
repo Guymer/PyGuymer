5601919e6693931ce8c104da3b3adfdd1667326f
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2017-02-28 19:47:15 +0000

    added fallback to ffprobe command (will now attempt a read as M-JPEG)

--- does_media_have_RTP_hints.py
+++ does_media_have_RTP_hints.py
@@ -19,7 +19,22 @@ def does_media_have_RTP_hints(fname = "missing"):
     )
     stdout, stderr = proc.communicate()
     if proc.returncode != 0:
-        raise Exception(u"\"ffprobe\" command failed")
+        # HACK: Fallback and attempt to load it as a raw M-JPEG stream.
+        proc = subprocess.Popen(
+            [
+                "ffprobe",
+                "-loglevel", "quiet",
+                "-print_format", "json",
+                "-show_streams",
+                "-f", "mjpeg",
+                fname
+            ],
+            stderr = subprocess.PIPE,
+            stdout = subprocess.PIPE
+        )
+        stdout, stderr = proc.communicate()
+        if proc.returncode != 0:
+            raise Exception(u"\"ffprobe\" command failed")
 
     # Loop over streams ...
     for stream in json.loads(stdout)[u"streams"]:
--- return_media_duration.py
+++ return_media_duration.py
@@ -19,7 +19,22 @@ def return_media_duration(fname):
     )
     stdout, stderr = proc.communicate()
     if proc.returncode != 0:
-        raise Exception(u"\"ffprobe\" command failed")
+        # HACK: Fallback and attempt to load it as a raw M-JPEG stream.
+        proc = subprocess.Popen(
+            [
+                "ffprobe",
+                "-loglevel", "quiet",
+                "-print_format", "json",
+                "-show_format",
+                "-f", "mjpeg",
+                fname
+            ],
+            stderr = subprocess.PIPE,
+            stdout = subprocess.PIPE
+        )
+        stdout, stderr = proc.communicate()
+        if proc.returncode != 0:
+            raise Exception(u"\"ffprobe\" command failed")
 
     # Return duration ...
     return float(json.loads(stdout)[u"format"][u"duration"])                    # [s]
