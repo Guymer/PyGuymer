81d510df3e06a85f0d5a966f0e1440bbffb56762
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2018-02-21 18:07:16 +0000

    added skeleton layout

--- MPLS/load_SubPlayItem.py
+++ MPLS/load_SubPlayItem.py
@@ -3,4 +3,22 @@
 def load_SubPlayItem(fobj, length2, length2a):
     # NOTE: see https://github.com/lerks/BluRay/wiki/SubPlayItem
 
-    raise Exception("load_SubPlayItem() is not implemented")
+    # Import modules ...
+    import struct
+
+    # Initialize variables ...
+    ans = {}
+    length2b = 0                                                                                                        # [B]
+
+    # Read the binary data ...
+    ans[u"Length"], = struct.unpack(u">H", fobj.read(2));                                                               length2 += 2; length2a += 2; length2b += 2
+
+    # NOTE: SubPlayItem is not implemented
+
+    # Pad out the read ...
+    if length2b != ans[u"Length"]:
+        l = ans[u"Length"] - length2b                                                                                   # [B]
+        fobj.read(l);                                                                                                   length2b += l
+
+    # Return answer ...
+    return ans, length2, length2a, length2b
